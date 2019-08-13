eac4f196f4cd27a0a4a3496c69017dc758f63ee7
Espen Klem <espen.klem@gmail.com>
2019-08-13 14:14:29 +0200

    DB init now works. Added to getDefaultData function

--- bookmarklet-app.js
+++ bookmarklet-app.js
@@ -1,65 +1,64 @@
 function getDefaultData() {
   services  = []
+  // Initiating DB
+  db = new Dexie("bookmarklets")
+  db.version(1).stores({
+    services: 'id,value'
+  }) 
   return { 
-    services
+    services,
+    db
   }
 }
 
-var bookmarklets = new Vue({
+let bookmarklets = new Vue({
   el: '#app',
   data: getDefaultData(),
   methods: {
-    // READ from db - always do when mounted()
-    // Populate services data model
+    // READ from db
     readFromDB: function() {
-      console.log('Reading from DB')
-      // loop through stuff
-      // JSON.parse strings of content
-      // ID = Object ?
-      // Populate data model
+      db.services.toArray().then (function(){
+        return db.services.toArray()
+      }).then(function(services) {
+        bookmarklets.services = stringParse(services)
+      }).catch(function(error) {
+        console.error(error)
+      })
+      
     },
+    
     // WRITE when user wants to create new or edit old
     // If ID not set, generate ID
     // Fire off a READ at the end
-    writeToDB: function() {
+    writeToDB: function(formInput) {
       console.log('Writing to DB')
+      db.services.put({id: '423lsdj4s', content: '{"name": "Zapier to Norch", "webhook": "https://hooks.zapier.com/hooks/catch/204265/"}'}).then (function(){
+        return db.services.toArray()
+      }).then(function (service) {
+        bookmarklets.services = service
+      }).catch(function(error) {
+        console.error(error)
+      })
     },
 
     // Delete ID
     // Fire off a READ at the end
-    deleteFromDB: function() {
+    deleteFromDB: function(id) {
       console.log('Deleting from DB')
     },
 
     // Show edit mode for ID and hide view mode
-    showEdit: function() {
+    showEdit: function(id) {
       console.log('Show edit')
     },
 
     // Hide edit mode for ID and show view mode
-    hideEdit: function() {
+    hideEdit: function(id) {
       console.log('Hide edit')
     }
   },
-  mounted() {
-    // setup and populate db
-    const db = new Dexie("bookmarklets")
-    db.version(1).stores({
-      services: 'id,value'
-    })
-    // put data to db
-    db.services.put({id: '423lsdj4s', content: '{"name": "Zapier to Norch", "webhook": "https://hooks.zapier.com/hooks/catch/204265/"}'}).then (function(){
-      //return db.services.get('s23lsdj4s')
-      return db.services.toArray()
-    }).then(function (service) {
-      // let zap = JSON.parse(service.content)
-      console.log(service)
-      // console.log(zap.name)
-      // console.log(zap.webhook)
-      bookmarklets.services = service
-    }).catch(function(error) {
-      console.error(error)
-    })
+  mounted: function() {
+    this.readFromDB()
   }
 })
 
@@ -70,3 +69,17 @@ var bookmarklets = new Vue({
 // - Stringify objects before storing
 
 // - Parse strings to objects before populating data model
+// loop through stuff
+const stringParse = function(array){
+  for (var i = 0; i < array.length; i++) {
+    // JSON.parse strings of content
+    array[i].content = JSON.parse(array[i].content)
+  }
+  return array
+}
+
+// Initiate DB
+const initiateDB = function() {
+  // setup and populate db
+  
+}
